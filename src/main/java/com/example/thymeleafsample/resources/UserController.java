9845edd30e3529ef31f5547f44be867eb7778a02
Kennedy Kariuki <kennedy.kariuki@tracom.co.ke>
2019-08-02 18:32:44 +0300

    thymleaf loop

--- src/main/java/com/example/thymeleafsample/resources/UserController.java
+++ src/main/java/com/example/thymeleafsample/resources/UserController.java
@@ -1,8 +1,13 @@
 package com.example.thymeleafsample.resources;
 
+import com.example.thymeleafsample.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -15,6 +20,16 @@ public class UserController {
         model.addAttribute("grade", grade);
         model.addAttribute("GPA", convertGPA(grade));
         return "demo";
+    }
+
+    @RequestMapping("demo2")
+    public String demo2(Model model) {
+        List<User> lst = new ArrayList<>();
+        lst.add(new User(1, "Tom", 30));
+        lst.add(new User(2, "Jerry", 29));
+        lst.add(new User(3, "Nancy", 27));
+        model.addAttribute("list", lst);
+        return "demo2";
     }
 
     private String convertGPA(double grade) {
